83c4e375821a58497122e506366a735861a89ac1
Dan <dannegru40@gmail.com>
2021-10-03 00:13:46 +0300

    shows & epsiodes getter for antena module

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 
 app.get("/live/:channel", async (req,res) => {
 
-    var body = {status: "", link: "", error: null}
+    var body = {status: "", link: ""}
 
     try {
         body.status = "OK"
@@ -27,7 +27,7 @@ app.get("/live/:channel", async (req,res) => {
 })
 app.get("/:module/live/:channel", (req,res) => {
 
-    var body = {status: "", link: "", error: null}
+    var body = {status: "", link: ""}
 
     try {
         if(req.params.module && modules.modules.includes(req.params.module)){
@@ -49,9 +49,74 @@ app.get("/:module/live/:channel", (req,res) => {
 
 })
 
+app.get("/:module/vod", async (req,res) => {
+    
+    var body = {status: "", link: ""}
+
+    try {
+        if(req.params.module && modules.modules.includes(req.params.module)){
+            body.status = "OK"
+            body.data = await modules.getVODlist(req.params.module)
+            res.json(body)
+        }else {
+            body.status = "ERROR"
+            body.data = null;
+            body.error = `Module ${req.params.module} not found`;
+            res.json(body);
+        }
+    } catch (error) {
+        body.status = "ERROR"
+        body.data = null;
+        body.error = error;
+        res.json(body)
+    }
+})
+app.get("/:module/vod/:show", async (req,res) => {
+    var body = {status: "", link: ""}
+
+    try {
+        if(req.params.module && modules.modules.includes(req.params.module)){
+            body.status = "OK"
+            body.data = await modules.getVOD(req.params.module, req.params.show, req.query.year, req.query.month)
+            res.json(body)
+        }else {
+            body.status = "ERROR"
+            body.data = null;
+            body.error = `Module ${req.params.module} not found`;
+            res.json(body);
+        }
+    } catch (error) {
+        body.status = "ERROR"
+        body.data = null;
+        body.error = error;
+        res.json(body)
+    }
+})
+app.get("/:module/vod/:show/:epid", async (req,res) => {
+    var body = {status: "", link: ""}
+
+    try {
+        if(req.params.module && modules.modules.includes(req.params.module)){
+            body.status = "OK"
+            body.link = await modules.getVOD_EP(req.params.module, req.params.show , req.params.epid)
+            res.json(body)
+        }else {
+            body.status = "ERROR"
+            body.link = null;
+            body.error = `Module ${req.params.module} not found`;
+            res.json(body);
+        }
+    } catch (error) {
+        body.status = "ERROR"
+        body.link = null;
+        body.error = error;
+        res.json(body)
+    }
+})
+
 app.post("/:module/login", express.urlencoded({extended: false}), async (req,res) => {
     let cookies;
-    let body = {status: "", cookies: "", error: null}
+    let body = {status: "", cookies: ""}
     let fs = require('fs');
     let file = fs.existsSync(`./modules/${req.params.module}.json`) ? fs.readFileSync(`./modules/${req.params.module}.json`).toString() : {auth : {username: req.body.username, password: req.body.password, cookies: null}, config: {}}
     let config = JSON.parse(file)

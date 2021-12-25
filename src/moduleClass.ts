1093ce074797649f4abdae7925149bc2b8edabe1
White Red <rednblkx@protonmail.com>
2021-12-25 21:38:50 +0200

    home route to list all valid modules, wildcard route to match if none do as as 404 kinda page, all channels scraper and bugfixes

--- src/moduleClass.ts
+++ src/moduleClass.ts
@@ -1,3 +1,4 @@
+import { writeFile } from "fs";
 class Module {
 
     MODULE_ID: string;
@@ -5,12 +6,14 @@ class Module {
     hasVOD: boolean;
     chList: string[];
     qualitiesList: string[];
+    initializeConfig: Function;
     liveChannels: Function;
     login: Function;
     getVOD: Function;
     getVOD_List: Function;
     getVOD_EP: Function;
     getVOD_EP_List: Function;
+    getChannels: Function;
     
     constructor(MODULE_ID: string, hasLive: boolean, hasVOD: boolean, chList: string[], qualitiesList?: string[]){
         this.MODULE_ID = MODULE_ID;
@@ -24,6 +27,26 @@ class Module {
         this.getVOD_List = dummy;
         this.getVOD_EP = dummy;
         this.getVOD_EP_List = dummy;
+        this.getChannels = dummy;
+        this.initializeConfig = async function initializeConfig(){
+            var config = {
+              "auth": {
+                  "username": "",
+                  "password": "",
+                  "cookies": null
+              },
+              "config": {
+                  "cache_enabled": true,
+                  "cachetime": 6,
+                  "chList": null
+              }
+            }
+            //write config to file
+            writeFile(`${__dirname}/../modules/${MODULE_ID}.json`, JSON.stringify(config, null, 2), () => {
+                console.log(`${MODULE_ID} config file created`);
+                return config;
+            });
+        };
 
         function dummy(): string {
             return "dummy function"

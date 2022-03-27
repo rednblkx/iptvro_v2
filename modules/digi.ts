1ca0efdd789d03233f92512f87329bc1a3f8ce68
Dan Negru <dannegru40@gmail.com>
2022-03-27 20:56:08 +0300

    basic module digi, class fixed a bit functions now implemented as methods instead of properties in class

---
+++ modules/digi.ts
@@ -0,0 +1,101 @@
+import Class from '../src/moduleClass.js';
+
+import axios from 'axios';
+import cheerio from 'cheerio';
+import puppeteer, {SerializableOrJSHandle} from 'puppeteer';
+import http2 from 'http2';
+
+var Module = new Class('digi', true, false)
+
+Module.login = async function login(username, password) {
+    let auth = Module.getAuth();
+    let reusecookie = {deviceId: ""};
+    auth.cookies && Module.logger('login',`reusing cookies ${auth.cookies}`);
+    return new Promise(async (resolve, reject) => {
+      try {
+        const browser = await puppeteer.launch({headless: !Module.debug, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const page = await browser.newPage();
+        await page.goto('https://www.digionline.ro/auth/login');
+        await page.evaluate((auth) => {
+            (<HTMLInputElement>document.getElementById("form-login-email")).value = auth.username;
+            (<HTMLInputElement>document.getElementById("form-login-password")).value = auth.password;
+            for(var a=0; a==document.getElementsByTagName("button").length - 1;a++){
+                if(document.getElementsByTagName("button")[a].getAttribute("type") === "submit"){
+                    document.getElementsByTagName("button")[a].click()
+                }
+                }
+        }, <SerializableOrJSHandle>(<unknown>auth))
+        if(auth.cookies){
+          auth.cookies.forEach(b => {
+            let aux = b.split("=");
+            reusecookie[aux[0]] = aux[1]
+          })
+          await page.setCookie({name: "deviceId", value: reusecookie.deviceId, domain:".digionline.ro"})
+        }    
+        await page.waitForSelector("#form-login-mode-all");
+        var pupcookie = await page.cookies();
+  
+        if(pupcookie.find(a => a.name == "deviceId")){
+          auth.cookies = [];
+          pupcookie.forEach(cookie => {
+            auth.cookies.push(`${cookie.name}=${cookie.value}`)
+          })
+          browser.close();
+          Module.setAuth({username: auth.username, password: auth.password, cookies: auth.cookies, lastupdated: new Date()})
+        }
+        else throw "No cookies found"
+        resolve(auth.cookies);
+      } catch (error) {
+        reject("digi| login: " + error);
+      }
+    });
+}
+
+Module.liveChannels = async function getFromDigi(channel) {
+    let config = Module.getConfig();
+    return new Promise(async (resolve, reject) => {
+      try {
+        Module.logger('liveChannels',"getting cookies");
+        let auth = Module.getAuth();
+        auth && Module.logger('liveChannels',"got cookies");
+        Module.logger('liveChannels',"getting the stream");
+        let play = await axios.post(
+          "https://www.digionline.ro/api/stream",
+          `id_stream=${config.chList[channel]["id"]}&quality=hq`,
+          {
+            headers: {
+              authority: "www.digionline.ro",
+              pragma: "no-cache",
+              "cache-control": "no-cache",
+              accept: "application/json, text/javascript, */*; q=0.01",
+              dnt: "1",
+              "x-requested-with": "XMLHttpRequest",
+              "user-agent":
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+              "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+              origin: "https:/www.digionline.ro",
+              "sec-fetch-site": "same-origin",
+              "sec-fetch-mode": "cors",
+              "sec-fetch-dest": "empty",
+              referer: `https:/www.digionline.ro/${config.chList[channel]["category"]}/${channel}`,
+              "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+              cookie: auth.cookies.join("; "),
+            },
+          }
+        );
+        play && Module.logger("liveChannels", "got the stream");
+        if(play.data.stream_errCode !== 0){
+            reject(Module.logger("liveChannels", `Got error from provider '${play.data.stream_err}'`, true))
+        }
+        resolve(play.data.stream_url);
+      } catch (error) {
+            
+        //   let auth = Module.getAuth();
+        //   Module.login(auth.cookies).then(() => {
+        //       getFromDigi(channel).then(stream => resolve(stream)).catch(er => reject(er))
+        //   }).catch(er => reject(er))
+      }
+    })
+  }
+
+export default Module;
--- src/moduleClass.ts
+++ src/moduleClass.ts
@@ -1,6 +1,6 @@
 import { existsSync, readFileSync, writeFile, writeFileSync } from "fs";
 
-type config = {
+type AuthConfig = {
     auth: {
         username: string;
         password: string;
@@ -13,14 +13,14 @@ type config = {
         chList: string[];
     }
 };
-class Module {
+
+class ModuleFunctions {
 
     MODULE_ID: string;
     hasLive: boolean;
     hasVOD: boolean;
     chList: string[];
     qualitiesList: string[];
-    initializeConfig: Function;
     liveChannels: Function;
     login: Function;
     getVOD: Function;
@@ -28,17 +28,86 @@ class Module {
     getVOD_EP: Function;
     getVOD_EP_List: Function;
     getChannels: Function;
-    getConfig: Function;
-    setConfig: Function;
-    getAuth: Function;
-    setAuth: Function;
+    debug: boolean;
+
+    initializeConfig(chList?: string[]){
+        var config = {
+          "auth": {
+              "username": "",
+              "password": "",
+              "cookies": null
+          },
+          "config": {
+              "cache_enabled": true,
+              "cachetime": 6,
+              "chList": chList || []
+          }
+        }
+        //write config to file
+        writeFileSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`, JSON.stringify(config, null, 2));
+        //log config
+        console.log(`initializeConfig| Config file created for module '${this.MODULE_ID}'`);
+    };
+
+    getAuth(): AuthConfig['auth'] {
+        let file = existsSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`) ? readFileSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`).toString() : null
+        let parsed : AuthConfig = file ? JSON.parse(file) : null;
+        if(!parsed){
+            throw "getAuth - Config file is not valid"
+        }else {
+            return parsed.auth;
+        }
+    }
+
+    setAuth(auth: {username: string, password: string, cookies: string[], lastupdated: Date}){
+        let file = existsSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`) ? readFileSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`).toString() : null
+        let parsed : AuthConfig = file ? JSON.parse(file) : null;
+        if(!parsed){
+            throw "setAuth - Config file is not valid"
+        }else {
+            parsed.auth = auth;
+            writeFile(`${process.cwd()}/modules/${this.MODULE_ID}.json`, JSON.stringify(parsed, null, 2), () => {
+                console.log(`${this.MODULE_ID} | config file updated - credentials changed`);
+            });
+        }
+    }
+
+    getConfig(): AuthConfig['config'] {
+        let file = existsSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`) ? readFileSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`).toString() : null
+        let parsed : AuthConfig = file ? JSON.parse(file) : null;
+        if(!parsed){
+            throw "getConfig - Config file is not valid"
+        }else {
+            return parsed.config
+        }
+    };
+
+    setConfig(key: string, value: any){
+        let file = existsSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`) ? readFileSync(`${process.cwd()}/modules/${this.MODULE_ID}.json`).toString() : null
+        let parsed : AuthConfig = file ? JSON.parse(file) : null;
+        if(!parsed){
+            throw "setConfig - Config file is not valid"
+        }else {
+            parsed.config[key] = value;
+            writeFile(`${process.cwd()}/modules/${this.MODULE_ID}.json`, JSON.stringify(parsed, null, 2), () => {
+                console.log(`${this.MODULE_ID} | config file updated - ${key} changed`);
+            });
+        }
+    };
+
+    logger(id: string, message: string, isError?: boolean) : string | Error {
+        if(this.debug){
+          console.log(`${this.MODULE_ID} - ${id}: ${message}`);
+        }
+        return isError ? new Error(`${this.MODULE_ID} - ${id}: ${message}`) : `${this.MODULE_ID} - ${id}: ${message}`
+      };
     
-    constructor(MODULE_ID: string, hasLive: boolean, hasVOD: boolean, chList: string[], qualitiesList?: string[]){
+    constructor(MODULE_ID: string, hasLive: boolean, hasVOD: boolean, chList?: string[], qualitiesList?: string[]){
         this.MODULE_ID = MODULE_ID;
         this.hasLive = hasLive;
         this.hasVOD = hasVOD;
-        this.chList = chList;
-        this.qualitiesList = qualitiesList;
+        this.chList = chList || null;
+        this.qualitiesList = qualitiesList || null;
         this.liveChannels = dummy;
         this.login = dummy;
         this.getVOD = dummy;
@@ -46,70 +115,7 @@ class Module {
         this.getVOD_EP = dummy;
         this.getVOD_EP_List = dummy;
         this.getChannels = dummy;
-        this.initializeConfig = async function initializeConfig(chList?: string[]){
-            var config = {
-              "auth": {
-                  "username": "",
-                  "password": "",
-                  "cookies": null
-              },
-              "config": {
-                  "cache_enabled": true,
-                  "cachetime": 6,
-                  "chList": chList || []
-              }
-            }
-            //write config to file
-            writeFileSync(`${process.cwd()}/modules/${MODULE_ID}.json`, JSON.stringify(config, null, 2));
-            //log config
-            console.log(`initializeConfig| Config file created for module '${MODULE_ID}'`);
-        };
-
-        this.getAuth = function getAuth(){
-            let file = existsSync(`${process.cwd()}/modules/${MODULE_ID}.json`) ? readFileSync(`${process.cwd()}/modules/${MODULE_ID}.json`).toString() : null
-            let parsed : config = file ? JSON.parse(file) : null;
-            if(!parsed){
-                throw "getAuth - Config file is not valid"
-            }else {
-                return parsed.auth;
-            }
-        }
-
-        this.setAuth = function setAuth(auth: {username: string, password: string, cookies: string[], lastupdated: Date}){
-            let file = existsSync(`${process.cwd()}/modules/${MODULE_ID}.json`) ? readFileSync(`${process.cwd()}/modules/${MODULE_ID}.json`).toString() : null
-            let parsed : config = file ? JSON.parse(file) : null;
-            if(!parsed){
-                throw "setAuth - Config file is not valid"
-            }else {
-                parsed.auth = auth;
-                writeFile(`${process.cwd()}/modules/${MODULE_ID}.json`, JSON.stringify(parsed, null, 2), () => {
-                    console.log(`${MODULE_ID} | config file updated - credentials changed`);
-                });
-            }
-        }
-
-        this.getConfig = function getConfig(key: string){
-            let file = existsSync(`${process.cwd()}/modules/${MODULE_ID}.json`) ? readFileSync(`${process.cwd()}/modules/${MODULE_ID}.json`).toString() : null
-            let parsed : config = file ? JSON.parse(file) : null;
-            if(!parsed){
-                throw "getConfig - Config file is not valid"
-            }else {
-                return parsed.config[key]
-            }
-        };
-
-        this.setConfig = function setConfig(key: string, value: any){
-            let file = existsSync(`${process.cwd()}/modules/${MODULE_ID}.json`) ? readFileSync(`${process.cwd()}/modules/${MODULE_ID}.json`).toString() : null
-            let parsed : config = file ? JSON.parse(file) : null;
-            if(!parsed){
-                throw "setConfig - Config file is not valid"
-            }else {
-                parsed.config[key] = value;
-                writeFile(`${process.cwd()}/modules/${MODULE_ID}.json`, JSON.stringify(parsed, null, 2), () => {
-                    console.log(`${MODULE_ID} | config file updated - ${key} changed`);
-                });
-            }
-        };
+        this.debug = (process.env.DEBUG?.toLowerCase() === 'true');
 
         function dummy(): string {
             return "dummy function"
@@ -118,4 +124,4 @@ class Module {
     }
 }
 
-export default Module;
+export default ModuleFunctions;

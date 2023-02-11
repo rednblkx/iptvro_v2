443025d1be0abd82444679961ec1371c77afdf50
Dan Negru <dannegru40@gmail.com>
2023-02-11 22:26:54 +0200

    pro-plus: refactored

--- src/modules/pro-plus.ts
+++ src/modules/pro-plus.ts
@@ -1,195 +1,233 @@
-import axios from "https://deno.land/x/axiod/mod.ts";
-import ModuleClass from "../moduleClass.ts";
-import { load } from "https://esm.sh/cheerio@1.0.0-rc.12";
+import axios from "https://deno.land/x/axiod@0.26.2/mod.ts";
+import ModuleClass, { ModuleType } from "../moduleClass.ts";
+import * as mod from "https://deno.land/std@0.91.0/encoding/base64.ts";
+import {
+  crypto,
+  toHashString,
+} from "https://deno.land/std@0.170.0/crypto/mod.ts";
+import { LoginResponse, LiveStreamResponse, LicenseRequestHeader, ChannelsList, VODList } from "./types/pro-plus.d.ts"
 
 /* A class that extends the Class class. */
-class ModuleInstance extends ModuleClass {
+class ModuleInstance extends ModuleClass implements ModuleType {
   constructor() {
     /* It calls the constructor of the parent class, which is `ModuleClass`. */
-    super("pro-plus", true, true, false);
+    super("pro-plus", true, false, true);
+  }
+
+  getVOD(show: string, authTokens: string[], page?: number | undefined): Promise<Record<string, unknown> | Record<string, unknown>[]> {
+    return Promise.reject(this.logger("getVOD", "Method not implemented", true))
+  }
+  getVOD_EP(show: string, epid: string, authTokens: string[]): Promise<string> {
+    return Promise.reject(this.logger("getVOD_EP", "Method not implemented", true))
   }
 
   /**
-   * It logs in to the service and returns the access token.
-   * @param {string} username - your username
-   * @param {string} password - string - the password you use to login to the app
-   * @returns The access token
+   * It takes a username and password, generates a random UUID, and then sends a POST request to the
+   * provider's login endpoint. If the response contains an access token, it saves the username,
+   * password, access token, and UUID to the database. If the response contains an error message, it
+   * throws an error
+   * @param {string} username - Your username
+   * @param {string} password - The password you use to login to the provider's website.
+   * @returns The access token and the uuid
    */
   async login(username: string, password: string): Promise<string[]> {
     try {
-      const step1 = await axios.post(
-        "https://protvplus.ro/login",
-        `email=${encodeURIComponent(username)}&password=${
-          encodeURIComponent(password)
-        }&login=Autentificare&_do=content11374-loginForm-form-submit`,
+      const uuid = crypto.randomUUID();
+      const login_res = await axios.post<LoginResponse & { message: string, code: string }>(
+        "https://apiprotvplus.cms.protvplus.ro/api/v2/auth-sessions", { username, password },
         {
           headers: {
             "Accept":
               "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Type": "application/json",
+            "X-Api-Key": "e09ea8e36e2726d04104d06216da2d3d9bc6c36d6aa200b6e14f68137c832a8369f268e89324fdc9",
+            "X-Device-Id": uuid,
+            "User-Agent": "PRO TV PLUS/1.15.0 (com.protvromania; build:2180; Android 12; Model:moto g(7) power okhttp/4.9.1",
+            "X-AppBuildNumber": "2180",
+            "X-Version": "1.15.0",
+            "X-DeviceName": "Moto G7 Power",
+            "X-DeviceModel": "moto g(7) power",
+            "X-DeviceManufacturer": "motorola",
+            "X-DeviceOSVersion": "32",
+            "X-DeviceOS": "Android",
+            "X-DeviceType": "mobile"
           },
-          responseType: "text",
-          redirect: "manual",
-          validateStatus: (status: number) => status === 302,
+          responseType: "json"
         },
       );
-      if (step1 && step1.data) {
-        this.logger(
-          "login",
-          `received response ${step1.data} , ${step1.headers}`,
-        );
-      }
-      if (step1.headers.get("set-cookie")) {
-        this.logger(
-          "login",
-          `received cookie ${step1.headers.get("set-cookie")}`,
-        );
+      this.logger("login", login_res.data)
+      if (login_res.data.credentials?.accessToken) {
         this.setAuth({
           username,
           password,
-          authTokens: step1.headers.get("set-cookie")?.split(";") || [],
+          authTokens: [login_res.data.credentials.accessToken, uuid],
           lastupdated: new Date(),
         });
         return Promise.resolve(
-          step1.headers.get("set-cookie")?.split(";") || [],
+          [login_res.data.credentials.accessToken, uuid],
         );
-      } else throw "No cookie received";
+      } else if (login_res.data.message) {
+        throw `Error from provider: ${login_res.data.message}`
+      }
+      return Promise.resolve([]);
     } catch (error) {
-      this.logger(
-        "login",
-        error.message || error.toString().substring(0, error.findIndex("\n")),
-      );
-      return Promise.reject(
-        error.message || error.toString().substring(0, error.findIndex("\n")),
-      );
+      return Promise.reject(this.logger(
+        "login", error, true
+      ));
     }
   }
 
   /**
-   * It gets the live stream of a channel.
-   * @param {string} id - the channel id, you can get it from the channel list
-   * @param {string[]} authTokens - The tokens you get from the login function.
-   * @param {Date} authLastUpdate - Date - The date when the auth tokens were last updated.
-   * @returns The stream url
+   * It takes the channel id, the auth tokens and the last time the auth tokens were updated and
+   * returns a promise that resolves to an object containing the stream url and the drm url and headers
+   * @param {string} id - The channel id
+   * @param {string[]} authTokens - The tokens returned by the login function.
+   * @param {Date} _authLastUpdate - Date - The date of the last time the auth tokens were updated.
+   * @returns A promise that resolves to an object with a stream and drm property.
    */
   async liveChannels(
     id: string,
     authTokens: string[],
     _authLastUpdate: Date,
-  ): Promise<{ stream: string; proxy?: string }> {
+  ): Promise<{ stream: string; drm?: { url: string, headers?: LicenseRequestHeader[] } }> {
     try {
       if (!authTokens) {
         const auth = await this.getAuth();
         this.logger("liveChannels", "No tokens, trying login");
         authTokens = await this.login(auth.username, auth.password);
       }
-      const channel = (await this.getConfig()).chList;
-      const step1 = await axios.get(
-        `https://protvplus.ro/tv-live/${id}-${
-          Object.keys(channel).find((key) => channel[key] === id)
-        }`,
+      const headers = {
+        "Authorization": `Bearer ${authTokens[0]}`,
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Content-Type": "application/json",
+        "X-Api-Key": "e09ea8e36e2726d04104d06216da2d3d9bc6c36d6aa200b6e14f68137c832a8369f268e89324fdc9",
+        "X-Device-Id": authTokens[1],
+        "User-Agent": "PRO TV PLUS/1.15.0 (com.protvromania; build:2180; Android 12; Model:moto g(7) power okhttp/4.9.1",
+        "X-AppBuildNumber": "2180",
+        "X-Version": "1.15.0",
+        "X-DeviceName": "Moto G7 Power",
+        "X-DeviceModel": "moto g(7) power",
+        "X-DeviceManufacturer": "motorola",
+        "X-DeviceOSVersion": "32",
+        "X-DeviceOS": "Android",
+        "X-DeviceType": "mobile"
+      };
+      const server_time = await axios.get<{ localTime: string, encoded: string }>("https://apiprotvplus.cms.protvplus.ro/api/v2/server/time", {
+        headers
+      })
+      this.logger("liveChannels", server_time);
+      const enc_string = new TextDecoder().decode(mod.decode("ZGtkZjM1ZzYhIHtjb250ZW50fXxwbGF5c3xuZzhyNWUzMSF8e3NlcnZlclRpbWV9ISNpM2R0JjQzQA==")).replace("{content}", id).replace("{serverTime}", server_time.data.localTime)
+      const hash = await crypto.subtle.digest(
+        "MD5",
+        new TextEncoder().encode(enc_string),
+      );
+      const live_channel = await axios.post<LiveStreamResponse & { message?: string, code?: string }>(
+        `https://apiprotvplus.cms.protvplus.ro/api/v2/content/${id}/plays?acceptVideo=hls%2Cdai%2Cdash%2Cdrm-widevine&t=${server_time.data.encoded}&s=${toHashString(hash)}`,
+        "",
         {
-          headers: {
-            accept:
-              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-            cookie: authTokens.join(";"),
-          },
+          headers
         },
       );
-      this.logger(
-        "liveChannels",
-        `received response ${step1.status} , ${step1.statusText}`,
-      );
-      let $ = load(step1.data);
-      // if(consoleL && $) console.log(`pro| getPlaylist: ${$(".live-iframe-wrapper.js-user-box")[0].attribs["data-url"]}`);
-      this.logger(
-        "liveChannels",
-        `getPlaylist: ${
-          $(".live-iframe-wrapper.js-user-box")[0].attribs["data-url"]
-        }`,
-      );
-      // if(consoleL) console.log(`pro| getPlaylist: getting channel's second link`);
-      this.logger("liveChannels", `getPlaylist: getting channel's second link`);
-      const step2 = await axios.get(
-        $(".live-iframe-wrapper.js-user-box")[0].attribs["data-url"],
-        {
-          headers: {
-            accept: "*/*",
-            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-            authorization: `Bearer undefined`,
-            "x-requested-with": "XMLHttpRequest",
-            cookie: authTokens.join(";"),
-            referrer: `https://protvplus.ro/tv-live/${id}-${
-              Object.keys(channel).find((key) => channel[key] === id)
-            }`,
-          },
-        },
-      );
-      // if(consoleL && step1.data) console.log(`pro| getPlaylist: got channel's second link`);
-      this.logger("liveChannels", `getPlaylist: got channel's second link`);
-      $ = load(step2.data);
-      // if(consoleL && $) console.log(`pro| getPlaylist: ${$("iframe").attr("src")}`);
-      this.logger("liveChannels", `getPlaylist: ${$("iframe").attr("src")}`);
-      // if(consoleL) console.log(`pro| getPlaylist: getting channel's stream URL`);
-      this.logger("liveChannels", `getPlaylist: getting channel's stream URL`);
-      const step3 = await axios.get($("iframe").attr("src") || "", {
-        headers: {
-          cookie: authTokens.join(";"),
-          referer: "https://protvplus.ro/tv-live/1-pro-tv",
-        },
-      });
-      // if(consoleL && step3.data) console.log(`pro| getPlaylist: got channel's stream`);
-      this.logger("liveChannels", `getPlaylist: got channel's stream`);
-      // if(consoleL && step3.data) console.log(`pro| getPlaylist: ${step3.data}`);
-      // if(consoleL && step3.data) console.log(`pro| getPlaylist: ${step3.data.match('{"HLS"(.*)}]}')}`);
-      const regex = /"HLS":.+?\s*:\s*["\']?([^"\'\s>]+)["\']?/g;
-      const stream = new URL(regex.exec(step3.data)?.[1] || "").href;
-      this.logger("liveChannels", `getPlaylist: ${stream}`);
-      // if(stream.includes("playlist-live_lq-live_mq-live_hq")){
-      //     stream = stream.replace("playlist-live_lq-live_mq-live_hq", "playlist-live_lq-live_mq-live_hq-live_fullhd");
-      // }
-      // if(consoleL && step3.data) console.log(`pro| getPlaylist: ${JSON.parse(step3.data.match('{"HLS"(.*)}]}')[0]).HLS[0].src}`);
-      // this.logger('liveChannels', `getPlaylist: ${stream}`)
-      return Promise.resolve({ stream });
+      this.logger("liveChannels", live_channel)
+      if (live_channel.data.message) {
+        return Promise.reject(this.logger("liveChannels", live_channel.data.message, true))
+      }
+      return Promise.resolve({ stream: live_channel.data.url, drm: { url: live_channel.data.drm.licenseUrl, headers: live_channel.data.drm.licenseRequestHeaders } })
     } catch (error) {
-      this.logger(
+      return Promise.reject(this.logger(
         "liveChannels",
-        error.message || error.toString().substring(0, error.findIndex("\n")),
-      );
-      return Promise.reject(
-        error.message || error.toString().substring(0, error.findIndex("\n")),
-      );
+        error,
+        true
+      ));
     }
   }
 
   /**
-   * It gets the HTML of the website, loads it into a cheerio object, then loops through all the channels
-   * and adds them to an object
-   * @returns A list of channels and their respective IDs
+   * It fetches the list of channels from the API and returns a dictionary of channel names and their
+   * respective IDs
+   * @returns A list of channels with their respective ids.
    */
-  async getChannels(): Promise<Record<string, unknown>> {
+  async getChannels(): Promise<Record<string, string>> {
     try {
-      const getHTML = (await axios.get("https://protvplus.ro/")).data;
-      const $ = load(getHTML);
-      const channelList: { [k: string]: string } = {};
-      $(".channels-main a").each(function (_i, el) {
-        const link = el.attribs["href"];
-        channelList[link.match(/([0-9])-(.*)/)?.[2] || 0] =
-          link.match(/([0-9])-(.*)/)?.[1] || "";
-      });
-      return Promise.resolve(channelList);
+      const channels = await axios.get<ChannelsList>("https://apiprotvplus.cms.protvplus.ro/api/v2/overview?category=livetv", {
+        headers: {
+          "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "Content-Type": "application/json",
+          "X-Api-Key": "e09ea8e36e2726d04104d06216da2d3d9bc6c36d6aa200b6e14f68137c832a8369f268e89324fdc9",
+          "X-Device-Id": crypto.randomUUID(),
+          "User-Agent": "PRO TV PLUS/1.15.0 (com.protvromania; build:2180; Android 12; Model:moto g(7) power okhttp/4.9.1",
+          "X-AppBuildNumber": "2180",
+          "X-Version": "1.15.0",
+          "X-DeviceName": "Moto G7 Power",
+          "X-DeviceModel": "moto g(7) power",
+          "X-DeviceManufacturer": "motorola",
+          "X-DeviceOSVersion": "32",
+          "X-DeviceOS": "Android",
+          "X-DeviceType": "mobile"
+        }
+      })
+      this.logger("getChannels", channels.data);
+      const list: { [k: string]: string } = {};
+      channels.data.liveTvs.forEach(obj => {
+        list[obj.name.normalize('NFKD').replace(/[^\w]/g, ' ').trim().replace(" ", "-").replace(" ", "").toLowerCase()] = obj.id;
+      })
+      return Promise.resolve(list)
     } catch (error) {
-      this.logger(
-        "getChannels",
-        error.message || error.toString().substring(0, error.findIndex("\n")),
-      );
       return Promise.reject(
-        error.message || error.toString().substring(0, error.findIndex("\n")),
+        this.logger(
+          "getChannels", error, true
+        )
+      );
+    }
+  }
+
+  async getVOD_List(authTokens: string[],page?: number|undefined): Promise<{ data: unknown[]; pagination?: { current_page: number; total_pages: number; per_page: number; }|undefined; }> {
+    try {
+      if (!(authTokens.length > 0) || typeof authTokens !== "object") {
+        const auth = await this.getAuth();
+        this.logger("getVOD", "No tokens, trying login");
+        authTokens = await this.login(auth.username, auth.password);
+      }
+      const vod_res = await axios.get<VODList>("https://apiprotvplus.cms.protvplus.ro/api/v2/overview", {
+        headers: {
+          "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "Content-Type": "application/json",
+          "X-Api-Key": "e09ea8e36e2726d04104d06216da2d3d9bc6c36d6aa200b6e14f68137c832a8369f268e89324fdc9",
+          "X-Device-Id": crypto.randomUUID(),
+          "User-Agent": "PRO TV PLUS/1.15.0 (com.protvromania; build:2180; Android 12; Model:moto g(7) power okhttp/4.9.1",
+          "X-AppBuildNumber": "2180",
+          "X-Version": "1.15.0",
+          "X-DeviceName": "Moto G7 Power",
+          "X-DeviceModel": "moto g(7) power",
+          "X-DeviceManufacturer": "motorola",
+          "X-DeviceOSVersion": "32",
+          "X-DeviceOS": "Android",
+          "X-DeviceType": "mobile"
+        }
+      })
+      this.logger("getVOD_List", vod_res.data);
+      let data: { name: string, img: string, link: string }[] = [];
+      vod_res.data.sections.forEach(obj => {
+        // obj.
+        data = obj.content.map(ep => {
+          return { name: ep.content.title, link: `/${this.MODULE_ID}/vod/${ep.content.id}`, img: ep.content.image }
+        })
+      })
+      return Promise.resolve({data});
+    } catch (error) {
+      return Promise.reject(
+        this.logger(
+          "getVOD_List", error, true
+        )
       );
     }
   }
 }
+
+
 
 /* It exports the class so it can be used in other files. */
 export default ModuleInstance;
---
+++ src/modules/types/pro-plus.d.ts
@@ -0,0 +1,232 @@
+export interface LiveStreamResponse {
+  url: string
+  streamProfiles: StreamProfile[]
+  alternativeAudioProfiles: AlternativeAudioProfile[]
+  videoType: string
+  watchedTime: number
+  content: Content
+  ads: Ads
+  drm: Drm
+  subtitles: string[]
+  properties: Property[]
+  playlist: string[]
+  tracking: Tracking
+  allowCasting: boolean
+  timeshiftMode: string
+  autoPlayControl: AutoPlayControl
+  autoPlayNext: string
+  nextVideoMode: string
+  productPlacement: ProductPlacement
+  isLive: boolean
+  length: number
+}
+
+interface StreamProfile {
+  name: string
+  resolutionWidth: number
+  resolutionHeight: number
+  drm: unknown
+}
+
+interface AlternativeAudioProfile {
+  name: string
+  languageCode: string
+  url: string
+}
+
+interface Content {
+  genres: unknown[]
+  languages: unknown
+  subtitles: unknown
+  properties: unknown
+  description: string
+  id: string
+  type: string
+  title: string
+  image: string
+  releaseDateLabel: string
+  labels: unknown[]
+  distributionMethod: string
+  geoRestriction: unknown
+  ageRestriction: number
+  isFavorite: boolean
+}
+
+interface Ads {
+  playlist: Playlist
+}
+
+interface Playlist {
+  type: string
+  data: string
+}
+
+interface Drm {
+  type: string
+  keySystem: string
+  licenseUrl: string
+  licenseRequestHeaders: LicenseRequestHeader[]
+  certificateUrl: string
+}
+
+interface LicenseRequestHeader {
+  name: string
+  value: string
+}
+
+interface Property {
+  name: string
+  value: string
+}
+
+interface Tracking {
+  productId: string
+  product: string
+}
+
+interface AutoPlayControl {
+  skipIntroMode: string
+  skipIntroOfferPosition: unknown
+  skipIntroOfferDuration: number
+  nextVideoAutoplayMode: string
+  nextVideoOfferPosition: NextVideoOfferPosition
+  nextVideoOfferDuration: number
+  skipNextVideoOfferWhenLengthIsLessThan: number
+  stopAutoplayWhenOnWifiAfter: number
+  stopAutoplayWhenOnMobileDataAfter: number
+}
+
+interface NextVideoOfferPosition {
+  type: string
+  value: number
+}
+
+interface ProductPlacement {
+  isEnabled: boolean
+  startShowDelay: number
+  midrollShowDelay: number
+  endShowOffset: number
+  displayTime: number
+  minShowInterval: number
+  location: string
+}
+
+
+interface User {
+  id: string;
+  username: string;
+  currentUserLocationCountry?: string;
+  data?: Record<string, unknown>;
+}
+
+interface Credentials {
+  accessToken: string;
+}
+
+export interface LoginResponse {
+  user: User;
+  credentials: Credentials;
+}
+
+export interface ChannelsList {
+  type: string
+  categories: unknown
+  browseAll: string
+  headline: unknown
+  sections: unknown
+  liveTvs: LiveTv[]
+}
+
+interface LiveTv {
+  id: string
+  name: string
+  logo: string
+  logoTransparent: string
+  currentlyPlaying: CurrentlyPlaying
+  nextShow: NextShow
+}
+
+interface CurrentlyPlaying {
+  timeslot: Timeslot
+  stream: Stream
+  id: string
+  type: string
+  title: string
+  image: string
+  releaseDateLabel: string
+  labels: unknown[]
+  distributionMethod: string
+  geoRestriction?: GeoRestriction
+  ageRestriction: number
+  isFavorite: boolean
+}
+
+interface Timeslot {
+  startAt: string
+  endAt: string
+}
+
+interface Stream {
+  isLive: boolean
+  length: number
+}
+
+interface GeoRestriction {
+  rule: string
+  countries: string[]
+}
+
+interface NextShow {
+  id: string
+  type: string
+  title: string
+  image: string
+  releaseDateLabel: string
+  labels: unknown[]
+  distributionMethod: string
+  geoRestriction?: GeoRestriction
+  ageRestriction: number
+  isFavorite: boolean
+}
+
+export interface VODList {
+  type: string
+  categories: unknown
+  browseAll: unknown
+  headline: Headline
+  sections: Section[]
+  liveTvs: unknown
+}
+
+interface Headline {
+  id: string
+  content: Content
+}
+
+interface Section {
+  id: string
+  name: string
+  type: string
+  content: Content2[]
+  watching: unknown
+  rented: unknown
+  live: unknown
+}
+
+interface Content2 {
+  id: string
+  content: Content3
+}
+
+interface Content3 {
+  id: string
+  type: string
+  title: string
+  image: string
+  releaseDateLabel: string
+  labels: unknown[]
+  distributionMethod: string
+  geoRestriction?: GeoRestriction
+  ageRestriction: number
+  isFavorite: boolean
+}
